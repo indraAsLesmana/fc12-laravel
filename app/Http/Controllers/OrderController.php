ecc92031993d5678fd17454540084b7ce9c2ca50
Indra Agus Lesmana <indra.lesmana07@homecredit.co.id>
2024-02-06 21:10:46 +0700

    implement send notification when status changes.

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -5,8 +5,11 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StoreOrderRequest;
 use App\Http\Requests\UpdateOrderRequest;
 use App\Models\Order;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Kreait\Firebase\Messaging\CloudMessage;
+use Kreait\Firebase\Messaging\Notification;
 
 class OrderController extends Controller
 {
@@ -34,6 +37,20 @@ class OrderController extends Controller
     }
 
     //update
+    // public function update(Request $request, $id)
+    // {
+    //     //update status order
+    //     $order = DB::table('orders')->where('id', $id);
+    //     $order->update([
+    //         'status' => $request->status,
+    //         'shipping_resi' => $request->shipping_resi,
+    //     ]);
+
+    //     //redirect to index
+    //     return redirect()->route('order.index');
+    // }
+
+    //update
     public function update(Request $request, $id)
     {
         //update status order
@@ -43,7 +60,29 @@ class OrderController extends Controller
             'shipping_resi' => $request->shipping_resi,
         ]);
 
+        //send notification to user
+        if ($request->status == 'on_delivery') {
+            $this->sendNotificationToUser($order->first()->user_id, 'Paket Dikirim dengan nomor resi ' . $request->shipping_resi);
+        }
+
         //redirect to index
         return redirect()->route('order.index');
+    }
+
+    public function sendNotificationToUser($userId, $message)
+    {
+        // Dapatkan FCM token user dari tabel 'users'
+
+        $user = User::find($userId);
+        $token = $user->fcm_id;
+
+        // Kirim notifikasi ke perangkat Android
+        $messaging = app('firebase.messaging');
+        $notification = Notification::create('Paket Dikirim', $message);
+
+        $message = CloudMessage::withTarget('token', $token)
+            ->withNotification($notification);
+
+        $messaging->send($message);
     }
 }
